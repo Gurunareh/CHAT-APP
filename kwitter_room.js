6442158d962187d4d1699f5f726d32000136dae1
Gurunareh <85010055+Gurunareh@users.noreply.github.com>
2022-01-16 19:55:14 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,18 @@
 
 //ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+      apiKey: "AIzaSyC2xb5iGx6trIIWeYIjGp9cskbx95kBntU",
+      authDomain: "practice-8ae4d.firebaseapp.com",
+      databaseURL: "https://practice-8ae4d-default-rtdb.firebaseio.com",
+      projectId: "practice-8ae4d",
+      storageBucket: "practice-8ae4d.appspot.com",
+      messagingSenderId: "502753078950",
+      appId: "1:502753078950:web:3c8c9c28fdab4516078e67"
+    };
+    
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
